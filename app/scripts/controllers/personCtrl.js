712229db6ec20b866b5c03b261318895e7bb5693
bujjibabu <bujji404@gmail.com>
2016-10-05 09:55:06 +0530

    edit object binding in html page

--- app/scripts/controllers/personCtrl.js
+++ app/scripts/controllers/personCtrl.js
@@ -13,7 +13,8 @@ angular.module('healthCareApp')
   	$rootScope.hideNavbar = false;
 
   	vm.addPerson = function (obj) {
-      console.log(obj)
+      console.log(obj);
+      ApiService.post(healthCareBusinessConstants.ADD, obj).then(successAddCallback, errorCallback).finally(finalCallBack);
     };
 
   	vm.searchEmployee = function () {
@@ -27,6 +28,10 @@ angular.module('healthCareApp')
       }
   	};    
 
+    //add user Success
+    function successAddCallback (response) {
+      console.log(response);
+    };
      // success Call back method
     function successCallback (response) {
       console.log(response);
@@ -35,8 +40,10 @@ angular.module('healthCareApp')
 
     // error call back method.
     function errorCallback (error) {
-      vm.errorMsg = error.data.message;
-      console.log("login response::", error);
+      if(error && error.data) {
+        vm.errorMsg = error.data.message;
+      }
+     console.log("login response::", error);
     };
 
      // final call back method called no matter success or failure
@@ -47,6 +54,202 @@ angular.module('healthCareApp')
 
   	vm.editEmployye = function (editObj) {
       $('a[data-target="#profile"]').tab('show');
+      vm.details = {
+  "employeeID": 1,
+  "empType": "Supervisor",
+  "field": "GSFSDFS SFSF",
+  "document": [
+    {
+      "documentID": "9",
+      "created_date": null,
+      "updated_date": null,
+      "created_id": null,
+      "updated_id": null,
+      "filename": null,
+      "filebytes": null
+    }
+  ],
+  "provider": {
+    "providerID": 1,
+    "specialty": "Cardio",
+    "npi": "Y",
+    "taxonomy": "H123455",
+    "credentials": "xxxxx",
+    "medlicense": {
+      "medLicState": "CA",
+      "medLicExpDate": "2016-09-29",
+      "created_date": "2016-09-29",
+      "updated_date": "2016-09-29",
+      "created_id": "rev",
+      "updated_id": "rev",
+      "medLicNumber": 12345678,
+      "medID": 1,
+      "document": [
+        {
+          "documentID": "3",
+          "created_date": null,
+          "updated_date": null,
+          "created_id": null,
+          "updated_id": null,
+          "filename": null,
+          "filebytes": null
+        },
+        {
+          "documentID": "4",
+          "created_date": null,
+          "updated_date": null,
+          "created_id": null,
+          "updated_id": null,
+          "filename": null,
+          "filebytes": null
+        }
+      ],
+      "state": "CA",
+      "expiryDate": "2016-09-29"
+    },
+    "dealicense": {
+      "deaLicName": "DEA LICENSE",
+      "deaLicAddress": null,
+      "deaLicTelephone": 0,
+      "deaLicFax": 0,
+      "deaLicEmail": null,
+      "deaLicExpDate": "2016-09-29",
+      "deaLicNumber": 34343535353,
+      "document": [
+        {
+          "documentID": "10",
+          "created_date": null,
+          "updated_date": null,
+          "created_id": null,
+          "updated_id": null,
+          "filename": null,
+          "filebytes": null
+        }
+      ],
+      "created_date": "2016-09-29",
+      "updated_date": "2016-09-29",
+      "created_id": "rev",
+      "updated_id": "rev",
+      "deaID": 1,
+      "address": {
+        "streetAddress": null,
+        "city": null,
+        "state": null,
+        "country": null,
+        "zip": 0,
+        "phoneNumber": null,
+        "faxNumber": null,
+        "emailID": null,
+        "created_date": null,
+        "updated_date": null,
+        "created_id": null,
+        "updated_id": null,
+        "addressID": 2
+      },
+      "name": "DEA LICENSE",
+      "email": null,
+      "fax": 0,
+      "deaaddress": null,
+      "telephone": 0,
+      "expiryDate": "2016-09-29"
+    },
+    "malprctlnce": {
+      "malPrctID": 1,
+      "malInsName": "MALPRATCICE INSURANCE",
+      "malInsAddress": null,
+      "malInsExpiryDate": "2016-09-29",
+      "malInsPolicyNumber": 1234567777,
+      "malInsPolicyDoc": [
+        {
+          "documentID": "5",
+          "created_date": null,
+          "updated_date": null,
+          "created_id": null,
+          "updated_id": null,
+          "filename": null,
+          "filebytes": null
+        },
+        {
+          "documentID": "7",
+          "created_date": null,
+          "updated_date": null,
+          "created_id": null,
+          "updated_id": null,
+          "filename": null,
+          "filebytes": null
+        }
+      ],
+      "malInsFaceSheet": [
+        {
+          "documentID": "8",
+          "created_date": null,
+          "updated_date": null,
+          "created_id": null,
+          "updated_id": null,
+          "filename": null,
+          "filebytes": null
+        }
+      ],
+      "created_date": "2016-09-29",
+      "updated_date": "2016-09-29",
+      "created_id": "rev",
+      "updated_id": "rev",
+      "address": {
+        "streetAddress": "WILBUR RD",
+        "city": "TO",
+        "state": "CA",
+        "country": "US",
+        "zip": 91360,
+        "phoneNumber": "805-111-2345",
+        "faxNumber": "805-111-2345",
+        "emailID": "RRRRR@gmail.com",
+        "created_date": "2016-09-29",
+        "updated_date": "2016-09-29",
+        "created_id": "rev",
+        "updated_id": "rev",
+        "addressID": 1
+      }
+    },
+    "created_date": "2016-09-29",
+    "updated_date": "2016-09-29",
+    "created_id": "rev",
+    "updated_id": "rev"
+  },
+  "created_date": "2016-09-29",
+  "updated_date": "2016-09-29",
+  "created_id": "rev",
+  "updated_id": "rev",
+  "person": {
+    "personID": 1,
+    "firstNm": "",
+    "lastNm": "SIV",
+    "middleNm": null,
+    "birthDt": "2016-09-29",
+    "gender": "M",
+    "status": "Active",
+    "empAddress": null,
+    "created_date": "2016-09-29",
+    "updated_date": "2016-09-29",
+    "created_id": "Rev",
+    "updated_id": "Rev",
+    "address": {
+      "streetAddress": "WILBUR RD",
+      "city": "TO",
+      "state": "CA",
+      "country": "US",
+      "zip": 91360,
+      "phoneNumber": "805-111-2345",
+      "faxNumber": "805-111-2345",
+      "emailID": "RRRRR@gmail.com",
+      "created_date": "2016-09-29",
+      "updated_date": "2016-09-29",
+      "created_id": "rev",
+      "updated_id": "rev",
+      "addressID": 3
+    },
+    "ssn": 12353546466
+  }
+};
   		console.log("Edit Employee Object::",editObj);      
   	};
 
@@ -55,7 +258,7 @@ angular.module('healthCareApp')
   		var temp = angular.copy(obj);
   		vm.users.splice(index,1);
   		$rootScope.loading = true;
-      ApiService.post(healthCareBusinessConstants.DELETE_URL, {empId: temp.empId}).then(deleteSuccessCallback, errorCallback).finally(finalCallBack);
+      ApiService.get(healthCareBusinessConstants.DELETE_URL + "?empID=" + temp.employeeID).then(deleteSuccessCallback, errorCallback).finally(finalCallBack);
   	};
 
   	// success Call back for delete method
--- app/scripts/services/constants.js
+++ app/scripts/services/constants.js
@@ -7,6 +7,6 @@ angular.module('healthCareApp').constant( 'healthCareBusinessConstants', {
   "STAFF": 'http://managementportal-env.us-east-1.elasticbeanstalk.com/hsm/secure/status/staff',
   "FACILITY": 'http://managementportal-env.us-east-1.elasticbeanstalk.com/hsm/secure/status/Facility',
   "DELETE_URL": 'http://managementportal-env.us-east-1.elasticbeanstalk.com/hsm/secure/employeeDelete',
-  "ADD": 'http://managementportal-env.us-east-1.elasticbeanstalk.com/hsm/employeeAdd',
-  "EDIT": 'http://managementportal-env.us-east-1.elasticbeanstalk.com/hsm/hsm/employeeEdit'
+  "ADD": 'http://managementportal-env.us-east-1.elasticbeanstalk.com/hsm/secure/employeeAdd',
+  "EDIT": ' http://managementportal-env.us-east-1.elasticbeanstalk.com/hsm/secure/employeeEdit'
  });
