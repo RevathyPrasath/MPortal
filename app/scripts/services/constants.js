5c07e97f3d60cfdd5331057ac0ac1782a12ec8c9
bujjibabu <bujji404@gmail.com>
2016-10-13 21:27:48 +0530

    Small changes for ui

--- app/scripts/services/constants.js
+++ app/scripts/services/constants.js
@@ -6,7 +6,7 @@ angular.module('healthCareApp').constant( 'healthCareBusinessConstants', {
   "SEARCH_URL": 'http://managementportal-env.us-east-1.elasticbeanstalk.com/hsm/secure/employeesearch',
   "STAFF": 'http://managementportal-env.us-east-1.elasticbeanstalk.com/hsm/secure/status/staff',
   "FACILITY": 'http://managementportal-env.us-east-1.elasticbeanstalk.com/hsm/secure/status/Facility',
-  "DELETE_EMP": 'http://managementportal-env.us-east-1.elasticbeanstalk.com/hsm/secure/employeeDelete',
+  "DELETE_EMP": 'http://managementportal-env.us-east-1.elasticbeanstalk.com/hsm/secure/employeeDelete?empID=',
   "ADD_EMPLOYEE": 'http://managementportal-env.us-east-1.elasticbeanstalk.com/hsm/secure/employeeAdd',
   "EDIT_EMPLOYEE": ' http://managementportal-env.us-east-1.elasticbeanstalk.com/hsm/secure/employeeEdit',
   'DELETE_DOC': 'http://managementportal-env.us-east-1.elasticbeanstalk.com/hsm/secure/deleteDocument?docID=',  
