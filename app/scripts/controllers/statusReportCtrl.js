bdf79ecca6da97882eb3d96169d2c97db80849dd
Shikha thakur <shikha121393@gmail.com>
2017-01-26 20:19:32 +0530

    "added view for person and statusReport component"

--- app/scripts/controllers/statusReportCtrl.js
+++ app/scripts/controllers/statusReportCtrl.js
@@ -8,34 +8,40 @@
  * Controller of the healthCareApp
  */
 angular.module('healthCareApp')
-	.controller('StatusReportCtrl', function ($rootScope, ApiService, healthCareBusinessConstants) {
-		var vm = this;
-		vm.expaire = function (value) {
-			$rootScope.loading = true;
-			if(value=='Staff') {
-	      ApiService.post(healthCareBusinessConstants.STAFF, {}).then(successCallback, errorCallback).finally(finalCallBack);
-			} else {
-       ApiService.post(healthCareBusinessConstants.FACILITY, {}).then(successCallback, errorCallback).finally(finalCallBack);
-			}
-		};
+    .controller('StatusReportCtrl', function($rootScope, ApiService, healthCareBusinessConstants) {
+        var vm = this;
+        vm.expaire = function(value) {
+            $rootScope.loading = true;
+            if (value == 'Staff') {
+                ApiService.post(healthCareBusinessConstants.STAFF, {}).then(successCallback, errorCallback).finally(finalCallBack);
+            } else {
+                ApiService.post(healthCareBusinessConstants.FACILITY, {}).then(successCallback, errorCallback).finally(finalCallBack);
+            }
+        };
 
-		// success Call back method
-    function successCallback (response) {
-      console.log(response);
-      vm.expaireData = response.data;
-    };
+        // success Call back method
+        function successCallback(response) {
+            console.log(response);
+            vm.expaireData = response.data;
+        };
 
-    // error call back method.
-    function errorCallback (error) {
-      vm.errorMsg = error.data.message;
-      console.log("login response::", error);
-    };
+        // error call back method.
+        function errorCallback(error) {
+            vm.errorMsg = error.data.message;
+            console.log("login response::", error);
+        };
 
-    // final call back method called no matter success or failure
-    function finalCallBack (res) {
-      console.log('finalCallBack', res);
-      $rootScope.loading = false;
-    };
-
-		vm.expaire('Staff');
-	});
+        // final call back method called no matter success or failure
+        function finalCallBack(res) {
+            console.log('finalCallBack', res);
+            $rootScope.loading = false;
+        };
+        vm.statusSelected = function(selectedTab) {
+            vm.selected = selectedTab;
+        }
+        vm.isActive = function(selectedTab) {
+            console.log(selectedTab);
+            return vm.selected == selectedTab
+        }
+        vm.expaire('Staff');
+    });
